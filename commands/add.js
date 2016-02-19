f1b47f55b1562d9d9c165d1782dbd9f0312d8059
Jared Grippe <jared@learnup.com>
2016-02-19 12:24:23 -0800

    refactored add command

--- commands/add.js
+++ commands/add.js
@@ -2,43 +2,52 @@ var async = require('async');
 
 module.exports = function(app){
 
-  app.command(['add( resource)? ([a-zA-Z]+)'], function(bot, message) {
+  app.command(['add( resource)? ([^\\s]+)'], function(bot, message) {
     var resourceName = message.match[2];
 
-    function ResourceAlreadyExistsError() {}
+    var ResourceAlreadyExistsError = function() {};
+
+    var queryForResource = function(cb) {
+      app.storage.resources.findOne({
+        name: resourceName
+      }, cb);
+    }
+
+    var errorIfResourceExists = function(resource, cb) {
+      if (resource) {
+        cb(new ResourceAlreadyExistsError);
+      }else{
+        cb();
+      }
+    };
+
+    var createResource = function(cb) {
+      app.storage.resources.save(resourceName, {
+        created_at: new Date(),
+      }, cb);
+    };
+
+    var respondWithSuccessMessage = function(resource, cb) {
+      bot.reply(message, "Great, I've added a resource named `" + resourceName + "`", cb)
+    };
+
+    var onError = function(err) {
+      if (err) {
+        if (err instanceof ResourceAlreadyExistsError){
+          bot.reply(message, "Sorry, it looks like there's an existing resource called `" + resourceName + "`");
+        }else{
+          console.error("Unexpected error:", err);
+          bot.reply(message, "Unexpected error: " + err);
+        }
+      }
+    };
 
     async.waterfall([
-      function(cb) {
-        app.storage.resources.findOne({
-          name: resourceName
-        }, cb);
-      },
-      function(resource, cb) {
-        if (resource) {
-          return cb(new ResourceAlreadyExistsError());
-        }
-
-        cb();
-      },
-      function(cb) {
-        app.storage.resources.save(resourceName, {
-          created_at: new Date(),
-        }, cb);
-      },
-      function(resource, cb) {
-        bot.reply(message, "Great, I've added a resource named `" + resourceName + "`", cb)
-      }
-    ], function(err) {
-      if (err && err instanceof ResourceAlreadyExistsError) {
-        bot.reply(message, "Sorry, it looks like there's an existing resource called `" + resourceName + "`");
-        return;
-      };
-
-      if (err) {
-        console.error("Unexpected error:", err);
-        return;
-      }
-    });
+      queryForResource,
+      errorIfResourceExists,
+      createResource,
+      respondWithSuccessMessage,
+    ], onError);
   });
 
 };
