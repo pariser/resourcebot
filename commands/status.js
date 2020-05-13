999ef2fc6026dfdd24f315cc4be234f9b56c0046
rreed <robin.reed@sonatus.com>
2020-05-13 15:37:08 -0700

    Large refactor of the STATUS command

--- commands/status.js
+++ commands/status.js
@@ -4,41 +4,43 @@ module.exports = function(app) {
   app.command('status( resource)? ([A-Za-z0-9\-]+) ([A-Za-z0-9\s]+)$', function(bot, message) {
     var resourceName = message.match[2];
     var newStatus = message.match[3];
+    var resource;
 
     var ResourceDoesNotExistError = function() {};
 
-    var queryForResource = function(cb) {
-      app.storage.resources.findOne({
-        name: resourceName
-      }, cb);
-    };
-
-    var errorIfResourceDoesNotExist = function(resource, cb) {
-      if (!resource) {
-        cb(new ResourceDoesNotExistError());
-      } else {
+    async.waterfall([
+      function(cb) {
+        app.storage.resources.findOne({
+          name: resourceName
+        }, cb);
+      },
+      function(_resource, cb) {
+        resource = _resource;
         cb();
-      }
-    };
-
-    var updateResource = function(resource, cb) {
+      },
+      function(resource, cb) {
+        if (!resource) {
+          cb(new ResourceDoesNotExistError());
+        } else {
+          cb();
+        }
+      },
+      function(resource, cb) {
         console.log(resource);
         console.log("claim_until", resource.claim_until);
         console.log("user", resource.user);
         console.log("username", resource.username);
-      app.storage.resources.save(resourceName, {
-        claim_until: resource.claim_until,
-        status: newStatus,
-        user: resource.user,
-        username: resource.username,
-      }, cb);
-    };
-
-    var respondWithSuccessMessage = function(resource, cb) {
-      bot.reply(message, 'Okay, I\'ve updated the status for `' + resourceName + '`', cb);
-    };
-
-    var onError = function(err) {
+        app.storage.resources.save(resourceName, {
+          claim_until: resource.claim_until,
+          status: newStatus,
+          user: resource.user,
+          username: resource.username,
+        }, cb);
+      },
+      function(resource, cb) {
+        bot.reply(message, 'Okay, I\'ve updated the status for `' + resourceName + '`', cb);
+      }
+    ], function(err) {
       if (err) {
         if (err instanceof ResourceDoesNotExistError) {
           bot.reply(message, 'Sorry, it looks like there\'s no existing resource called `' + resourceName + '`');
@@ -47,13 +49,6 @@ module.exports = function(app) {
           bot.reply(message, 'Unexpected error: ' + err);
         }
       }
-    };
-
-    async.waterfall([
-      queryForResource,
-      errorIfResourceDoesNotExist,
-      updateResource,
-      respondWithSuccessMessage
-    ], onError);
+    });
   });
 };
